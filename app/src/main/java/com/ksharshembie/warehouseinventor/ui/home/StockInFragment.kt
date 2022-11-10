78da2f6d4f2fe8482049aefc262eade41cca8283
ksharshembie <kadyrbek.sharshembiev@gmail.com>
2022-11-10 17:24:52 +0600

    Zxing barcode scanner added

--- app/src/main/java/com/ksharshembie/warehouseinventor/ui/home/StockInFragment.kt
+++ app/src/main/java/com/ksharshembie/warehouseinventor/ui/home/StockInFragment.kt
@@ -1,11 +1,16 @@
 package com.ksharshembie.warehouseinventor.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
 import com.ksharshembie.warehouseinventor.databinding.FragmentStockInBinding
+
 
 class StockInFragment : Fragment() {
 
@@ -14,9 +19,41 @@ class StockInFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentStockInBinding.inflate(LayoutInflater.from(context), container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnScan.setOnClickListener {
+            scanCode()
+        }
+    }
+
+    private fun scanCode() {
+        val options = ScanOptions()
+        //options.setDesiredBarcodeFormats(ScanOptions.ONE_D_CODE_TYPES)
+        options.setPrompt("Scan a barcode")
+        //options.setCameraId(0) // Use a specific camera of the device
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        barcodeLauncher.launch(options)
+    }
+
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(requireActivity(), "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                requireActivity(),
+                "Scanned: " + result.contents,
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
 }
